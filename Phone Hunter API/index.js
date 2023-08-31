7a488345b5d1e19e3468f49280ad5a0225ba93ea
Iqbal <iqbalhasan6116@gmail.com>
2023-08-31 06:26:16 +0600

    Added phone hunter api

--- Phone Hunter API/index.js
+++ Phone Hunter API/index.js
@@ -1,10 +1,33 @@
-const loadPhone = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
-    const datas = await res.json();
-    const phones = datas.data;
+const button = document.querySelector("#button");
+const input = document.querySelector("#input");
+button.addEventListener("click", function () {
+  let searchResult = input.value;
+  input.value = "";
+  loadData(searchResult);
+});
 
-    for (const phone of phones) {
-        console.log(phone);
-    }
-}
-loadPhone();
+const loadData = async (searchResult) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchResult}`
+  );
+  const datas = await res.json();
+  const phones = datas.data;
+
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  for (const phone of phones) {
+
+    const div = document.createElement("div");
+    div.classList.add("main");
+    div.innerHTML = `
+        <div class="img-div">
+            <img src="${phone.image}" alt="">
+        </div>
+        <div class="text-div">
+            <h1 class="h1">${phone.phone_name}</h1>
+        </div>
+        `;
+    container.appendChild(div);
+  }
+};
+loadData("iphone");
